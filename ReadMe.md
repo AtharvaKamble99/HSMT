b9f25c8ed3d6b2e6e4763f3654eea5557c5000bb
AtharvaKamble99 <atharvakamble99@gmail.com>
2025-10-28 00:30:05 +0530

    Model of the db designed and Created

---
+++ ReadMe.md
@@ -0,0 +1,1 @@
+This is a practise Hospital Management System Project being developed to learn the basics of Java Springboot 
---
+++ src/test/java/com/example/HMST/HmstApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.HMST;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HmstApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
